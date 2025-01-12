b423145b54c1361c840a4210111fe19e76e0a5c8
IditOksman <iditush303@gmail.com>
2025-01-12 23:39:06 +0200

    angular commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+
+interface ApiResponse {
+  data: any;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, CommonModule, HttpClientModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'angular_client';
+  title: string = 'nushka';
+  responseData: any;
+
+  constructor(private http: HttpClient) {}
+
+  getData() {
+    this.http
+      .get<ApiResponse>('http://localhost:3000/api/python-data')
+      .subscribe(
+        (response) => {
+          this.responseData = response;
+          console.log('Response:', response);
+        },
+        (error) => {
+          console.error('Error:', error);
+        }
+      );
+  }
 }
